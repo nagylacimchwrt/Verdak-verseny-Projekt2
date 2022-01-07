c55bcb79879cf2a85c9d624b04f9eb515d79aba5
nagylacimchwrt <92784296+nagylacimchwrt@users.noreply.github.com>
2022-01-07 20:07:28 +0100

    Projekt létrehozás
    A projekt létrehozása megtörtént.

---
+++ Nagy_Laszlo_11B_Projekt2/Nagy_Laszlo_11B_Projekt2/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Nagy_Laszlo_11B_Projekt2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
