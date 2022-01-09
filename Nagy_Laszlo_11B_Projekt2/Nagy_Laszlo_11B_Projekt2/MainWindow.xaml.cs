12e9e7e9749c96f83fefb8fe4565844a9f0d4fd6
nagylacimchwrt <92784296+nagylacimchwrt@users.noreply.github.com>
2022-01-09 22:12:09 +0100

    Start gomb működik
    Működik a gomb, kattintás után nem lehet újból rákattintani, és a többi gombra sem. Jöhet a mozgásra bírás.

--- Nagy_Laszlo_11B_Projekt2/Nagy_Laszlo_11B_Projekt2/MainWindow.xaml.cs
+++ Nagy_Laszlo_11B_Projekt2/Nagy_Laszlo_11B_Projekt2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Nagy_Laszlo_11B_Projekt2
 {
@@ -20,11 +21,72 @@ namespace Nagy_Laszlo_11B_Projekt2
     /// </summary>
     public partial class MainWindow : Window
     {
-        int szamlalo = new int();
         
+        Verda verda1;
+        Verda verda2;
+        Verda verda3;
+        Verseny verseny;
+        DispatcherTimer idozito;
+        szelepKupa szelepkupa;
+        Random rnd = new Random();
+        bool mehet = false;
+        int v = 100;
+        double celErtek;
+
         public MainWindow()
         {
             InitializeComponent();
+            celErtek = celVonal.Margin.Left;
+            szelepkupa = new szelepKupa();
+
+            idozito = new DispatcherTimer();
+            idozito.Interval = TimeSpan.FromMilliseconds(100);
+            idozito.Start();
+            idozito.Tick += Tick;
+            ujFutamGomb.IsEnabled = false;
+            ujBajnoksagGomb.IsEnabled = false;
+
+            verseny = new Verseny();
+            verda1 = new Verda();
+            verda2 = new Verda();
+            verda3 = new Verda();
+
         }
+        private void startGomb_Click(object sender, RoutedEventArgs e)
+        {
+            mehet = true;
+            startGomb.IsEnabled = false;
+            ujBajnoksagGomb.IsEnabled = false;
+            ujFutamGomb.IsEnabled = false;
+        }
+
+        private void Tick (object sender, EventArgs e)
+        {
+            if (!mehet) return;
+            int rnd1 = rnd.Next(1, 3);
+            int rnd2 = rnd.Next(1, 3);
+            int rnd3 = rnd.Next(1, 3);
+        }
+
+        public class Verda
+        {
+
+        }
+
+        public class Verseny
+        {
+
+        }
+
+        public class szelepKupa
+        {
+
+        }
+
+        public void Mozgas(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
