c0b2c5d6070f11e7a6a3ae9983001613b8b69590
nagylacimchwrt <92784296+nagylacimchwrt@users.noreply.github.com>
2022-01-10 19:21:24 +0100

    A versenyzők mozognak
    Random "versenyző" ér be a célba. Nem teljesen jó, a célvonalon túl mennek.

--- Nagy_Laszlo_11B_Projekt2/Nagy_Laszlo_11B_Projekt2/MainWindow.xaml.cs
+++ Nagy_Laszlo_11B_Projekt2/Nagy_Laszlo_11B_Projekt2/MainWindow.xaml.cs
@@ -36,20 +36,23 @@ namespace Nagy_Laszlo_11B_Projekt2
         public MainWindow()
         {
             InitializeComponent();
-            celErtek = celVonal.Margin.Left;
+            celErtek = celVonal.Margin.Right;
             szelepkupa = new szelepKupa();
 
             idozito = new DispatcherTimer();
-            idozito.Interval = TimeSpan.FromMilliseconds(100);
+            idozito.Interval = TimeSpan.FromMilliseconds(50);
             idozito.Start();
             idozito.Tick += Tick;
             ujFutamGomb.IsEnabled = false;
             ujBajnoksagGomb.IsEnabled = false;
 
             verseny = new Verseny();
-            verda1 = new Verda();
-            verda2 = new Verda();
-            verda3 = new Verda();
+            verda1 = new Verda("verda1", mcQueen, verseny);
+            verda2 = new Verda("verda2", Matuka, verseny);
+            verda3 = new Verda("verda3", docHudson, verseny);
+            verseny.verdak.Add(verda1);
+            verseny.verdak.Add(verda2);
+            verseny.verdak.Add(verda3);
 
         }
         private void startGomb_Click(object sender, RoutedEventArgs e)
@@ -66,27 +69,57 @@ namespace Nagy_Laszlo_11B_Projekt2
             int rnd1 = rnd.Next(1, 3);
             int rnd2 = rnd.Next(1, 3);
             int rnd3 = rnd.Next(1, 3);
+
+            verda1.Mozgas(celErtek, rnd1);
+            verda2.Mozgas(celErtek, rnd2);
+            verda3.Mozgas(celErtek, rnd3);
         }
 
         public class Verda
         {
+            public string name;
+            public int pontSzam = 0;
+            public Rectangle tgl;
+            public int elsoHelyekSzama = 0;
+            public int masodikHelyekSzama = 0;
+            public int harmadikHelyekSzama = 0;
+            Verseny verseny;
+            public Thickness start;
+            public float speed;
+            public float value = 5f;
 
+            public Verda(string name, Rectangle tgl, Verseny verseny)
+            {
+                this.verseny = verseny;
+                Random rnd = new Random();
+                this.tgl = tgl;
+                this.name = name;
+                speed = rnd.Next(1, 10);
+                start = tgl.Margin;
+            }
+
+            public void Mozgas(double celErtek, int rnd)
+            {
+                if (tgl.Margin.Left >= celErtek)
+                {
+                    tgl.Margin = new Thickness(celErtek, tgl.Margin.Top, 0, 0);
+                }
+
+                Thickness th = new Thickness(value, tgl.Margin.Top, 0, 0);
+                tgl.Margin = th;
+
+                value += 5 * rnd;
+            }
         }
 
         public class Verseny
         {
-
+            public List<Verda> verdak = new List<Verda>();
         }
 
         public class szelepKupa
         {
 
         }
-
-        public void Mozgas(object sender, EventArgs e)
-        {
-
-        }
-
     }
 }
